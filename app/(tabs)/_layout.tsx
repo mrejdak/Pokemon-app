dd1f847575e39c4c94cb79aafebca69eea6e3367
Michal <michal.rejdak@swmansion.com>
2025-07-04 08:53:27 +0200

    Created tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,41 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+export default function TabsLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "#e91e63",
+                tabBarInactiveTintColor: "#000",
+            }}>
+            <Tabs.Screen
+                name="favourite"
+                options={{
+                    title: "Favourite",
+                    tabBarLabel: "Favourite",
+                }}
+            />
+            <Tabs.Screen
+                name="infiniteScroll"
+                options={{
+                    title: "Pokémon List",
+                    tabBarLabel: "Pokémon",
+                }}
+            />
+            <Tabs.Screen
+                name="visionCamera"
+                options={{
+                    title: "Camera",
+                    tabBarLabel: "Camera",
+                }}
+            />
+            <Tabs.Screen
+                name="map"
+                options={{
+                    title: "Map",
+                    tabBarLabel: "Map",
+                }}
+            />
+        </Tabs>
+    )
 }
---
+++ app/(tabs)/infiniteScroll.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit infiniteScroll.tsx to edit this screen. Or don&apos;t.</Text>
+    </View>
+  );
+}
