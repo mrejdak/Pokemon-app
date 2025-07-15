612bec2726f46a6f26998f7331f11796778b5807
Michal <michal.rejdak@swmansion.com>
2025-07-15 13:48:22 +0200

    added camera & face detection

--- app/(tabs)/visionCamera.tsx
+++ app/(tabs)/visionCamera.tsx
@@ -1,13 +1,28 @@
-import { useEffect } from "react";
-import { Text, View } from "react-native";
-import { useCameraDevice, useCameraPermission } from "react-native-vision-camera";
+import { useIsFocused } from "@react-navigation/native";
+import { useEffect, useRef } from "react";
+import { AppState, StyleSheet, Text, View } from "react-native";
+import { Frame, useCameraDevice, useCameraPermission } from "react-native-vision-camera";
+import { Camera, Face, FaceDetectionOptions } from "react-native-vision-camera-face-detector";
 
-export default function Camera() {
+export default function VisionCamera() {
   const device = useCameraDevice('front')
   const { hasPermission, requestPermission } = useCameraPermission()
+  const isFocused = useIsFocused()
+  const isActive = isFocused && AppState.currentState === "active"
 
   useEffect(() => {requestPermission()}, [])
-  
+
+  const faceDetectionOptions = useRef<FaceDetectionOptions>( {
+    landmarkMode: 'all'
+  } ).current
+
+  const handleFaceDetection = (faces: Face[], frame: Frame) => {
+    console.log(
+      'faces', faces.length,
+      'frame', frame.toString()
+    )
+    // TODO: draw pokemon on faces
+  }
   
   if (!hasPermission) return (
     <View
@@ -20,9 +35,23 @@ export default function Camera() {
     >
       <Text>Masz poksa na czole. Serio. A teraz dej permisje</Text>
     </View>
-    
   );
-  return <View style={{justifyContent: 'center', flex: 1, alignItems: 'center'}}><Text>masz</Text></View>
-  
 
+  if (device === undefined) return (<View style={styles.deviceless}>
+    <Text>Where kamera</Text>
+  </View>)
+
+  return (<Camera style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={isActive}
+      enableFpsGraph={true}
+      faceDetectionCallback={handleFaceDetection}
+      faceDetectionOptions={faceDetectionOptions}/>)
 }
+
+const styles = StyleSheet.create({
+  deviceless: {
+    justifyContent: 'center',
+    alignContent: 'center'
+  }
+})
